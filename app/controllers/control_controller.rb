a7cf3075c9bd692a7ce8f5eca74749fca651ac5a
James <james_knowles@pacbell.net>
2013-01-02 12:01:15 -0800

    dev

--- app/controllers/control_controller.rb
+++ app/controllers/control_controller.rb
@@ -5,14 +5,26 @@ class ControlController < ApplicationController
 		status = ""
 
 		if params.has_key?(:capturestatus)
+			status = ""
+			all_ok = true
 			path = "/home/deploy/gnip/streaming"
-			latest = Dir.entries(path).map{|f| File.mtime(path + "/" + f)}.max
-			status = "Most recent capture time is #{Time.now-latest} seconds old\n\n"
-			if latest and Time.now - latest < 5*60
-				status << "Capture OK\n"
+			latest_pt  = Dir.entries(path).glob("*.json").map{|f| File.mtime(path + "/" + f)}.max
+			latest_edc = Dir.entries(path).glob("*.xml").map{|f| File.mtime(path + "/" + f)}.max
+			status << "Most recent PT  capture time is #{Time.now-latest_pt} seconds old\n"
+			status << "Most recent EDC capture time is #{Time.now-latest_edc} seconds old\n\n"
+			if latest_pt and Time.now - latest_pt < 5*60
+				status << "PT Capture OK\n"
 			else
-				status << "Capture FAIL\n"
+				status << "PT Capture FAIL\n"
+				all_ok = false
 			end
+			if latest_edc and Time.now - latest_edc < 8*60*60
+				status << "EDC Capture OK\n"
+			else
+				status << "EDC Capture FAIL\n"
+				all_ok = false
+			end
+			status << "\nALL OK"
 		end
 
 		respond_to do |format|
